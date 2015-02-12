03d6aa2a8714e91ba7bba3d58fdda26ac25a712c
Dan Lazewatsky <dan@lazewatsky.com>
2015-02-12 11:51:55 -0500

    do basic auth correctly

--- src/peac_bridge/peac_client.py
+++ src/peac_bridge/peac_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import json
 import requests
-# import requesocks as requests
+from requests.auth import HTTPBasicAuth
 import urlparse
 import time
 
@@ -25,19 +25,20 @@ class PEAC(object):
     def __init__(self, server, user, password, proxies={}):
         self.server = server
         self.authstr = '%s:%s' % (user,password)
+        self.user = user
+        self.password = password
         self.proxies = proxies
 
     def _make_url(self, peacinfo):
         urlparts = list(urlparse.urlparse(self.server + peacinfo.url))
-        urlparts[1] = self.authstr + '@' + urlparts[1]
         return urlparse.urlunparse(urlparts)
 
     def _PEAC_request(self, peacinfo, payload=None, url_args=dict()):
         url = self._make_url(peacinfo)
         if payload:
-            resp = requests.request(peacinfo.method, url % url_args, data=json.dumps(payload), headers=peacinfo.headers, proxies=self.proxies)
+            resp = requests.request(peacinfo.method, url % url_args, data=json.dumps(payload), headers=peacinfo.headers, auth=HTTPBasicAuth(self.user, self.password), proxies=self.proxies)
         else:
-            resp = requests.request(peacinfo.method, url % url_args, headers=peacinfo.headers, proxies=self.proxies)
+            resp = requests.request(peacinfo.method, url % url_args, headers=peacinfo.headers, auth=HTTPBasicAuth(self.user, self.password), proxies=self.proxies)
         return resp
 
     def list_locations(self):
@@ -70,7 +71,7 @@ class PEAC(object):
         '''
         return self._PEAC_request(CONTROLS_INFO, url_args=dict(deviceId=device_id)).json()
 
-    def update_control(self, id, numval):
+    def update_control(self, controlId, numval):
         '''
         Updates the control value. This call is used to 'press' a button.
 
@@ -78,7 +79,8 @@ class PEAC(object):
         Params: JSON Control Update Object
         Response: Control object
         '''
-        return self._PEAC_request(UPDATE_INFO, payload=dict(id=id, numVal=numval)).json()
+        # import pdb; pdb.set_trace()
+        return self._PEAC_request(UPDATE_INFO, payload=dict(id=controlId, numVal=numval)).json()
 
 
 def test_server_responses():
