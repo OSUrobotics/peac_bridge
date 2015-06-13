dd9ed4c58ef987ba463850d6fcad0d7e9a68d324
Dan Lazewatsky <dan@lazewatsky.com>
2015-06-13 11:34:36 -0500

    publish actuation on topic

--- scripts/actuation_logging_server.py
+++ scripts/actuation_logging_server.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 from peac_bridge.msg import ControlActuation
-from peac_bridge.srv import ReportControlActuation
+from peac_bridge.srv import ReportControlActuation, ReportControlActuationResponse
 import rospy
 
 control_pub = None
 
-def handle_control_actuated(req):
-    import pdb; pdb.set_trace()  # breakpoint 3d406f34 //
 
+def handle_control_actuated(req):
+    req.actuation.header.stamp = rospy.Time.now()
+    req.actuation.header.frame_id = 'base_link'
+    control_pub.publish(req.actuation)
+    return ReportControlActuationResponse()
 
 if __name__ == '__main__':
     rospy.init_node('actuation_logging_server')
     control_pub = rospy.Publisher('/control_actuated', ControlActuation)
     rospy.Service('~report_control_actuated', ReportControlActuation, handle_control_actuated)
+    rospy.spin()
